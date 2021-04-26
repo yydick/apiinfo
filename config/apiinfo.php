3664cb74983a53d46188bd166ad7fd8e96b8f738
yydick <yydick@sohu.com>
2021-04-26 18:59:21 +0800

    提交变更

---
+++ config/apiinfo.php
@@ -0,0 +1,19 @@
+<?php
+
+//ApiInfo配置文件
+
+return [
+    'default' => [
+        /**
+         * 要扫描的路由前缀
+         */
+        'prefix' => ['api'],
+        /**
+         * 要扫描的路由命名空间, 暂时保留
+         */
+        'namespace' => '',
+        'cachePath' => storage_path(),
+        'cacheTimeOut' => 0,
+        ''
+    ],
+];
